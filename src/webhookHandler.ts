81f13eca734fffca26ce34d40d13f846257a0148
ProdigyDoge <166218071+ProdigyDoge@users.noreply.github.com>
2024-06-17 16:25:46 -0500

    Update webhookHandler.ts

--- src/webhookHandler.ts
+++ src/webhookHandler.ts
@@ -17,7 +17,7 @@ function isWebhookConfigured() {
     return !!getConfigProperty('WEBHOOK_URL')
 }
 
-export function sendWebhookInitialized() {
+export function sendWebhookInitialized(ID: string) {
     if (!isWebhookConfigured()) {
         return
     }
@@ -27,15 +27,21 @@ export function sendWebhookInitialized() {
         embeds: [
             {
                 title: 'Initialized Connection',
+                color: 7714666,
                 fields: [
                     { name: 'Connected as:', value: `\`\`\`${ingameName}\`\`\``, inline: false },
                     {
                         name: 'Started at:',
                         value: `<t:${(Date.now() / 1000).toFixed(0)}:t>`,
                         inline: false
-                    }
+                    },
+                    { name: 'Your Connection ID:', value: `\`\`\`${ID}\`\`\`` , inline: false},
                 ],
-                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` }
+                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` },
+                footer: {
+                    "text": "Edit Made by luizfernando13",
+                    "icon_url": "https://cdn.discordapp.com/avatars/591035486693359641/a363e32ab636d378d46de06a1e6d6616.png?size=2048"
+                  }
             }
         ]
     })
@@ -50,6 +56,7 @@ export function sendWebhookItemPurchased(itemName: string, price: string, whitel
         embeds: [
             {
                 title: 'Item Purchased',
+                color: 570644,
                 fields: [
                     {
                         name: 'Item:',
@@ -60,22 +67,56 @@ export function sendWebhookItemPurchased(itemName: string, price: string, whitel
                         name: 'Bought for:',
                         value: `\`\`\`${price}\`\`\``,
                         inline: true
-                    }
+                    },
+                    {
+                        name: 'Estimated Profit:',
+                        value: `\`\`\`${profit}\`\`\``,
+                        inline: false,
+                    },
+                    { name:' ', value:`<t:${(Date.now() / 1000).toFixed(0)}:R>`, inline: false}
                 ],
-                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` }
+                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` },
             }
         ]
-    }
+    })
+}
 
-    if (whitelistedData) {
-        webhookData.embeds[0].fields.push({
-            name: 'Whitelist match:',
-            value: `\`\`\`${whitelistedData.reason}\`\`\``,
-            inline: false
-        })
+export function sendWebhookItemPurchased100M(itemName: string, price: string, profit: string) {
+    if (!isWebhookConfigured()) {
+        return
     }
-
-    sendWebhookData(webhookData)
+    let ingameName = getConfigProperty('INGAME_NAME')
+    sendWebhookData({
+        embeds: [
+            {
+                title: 'Legendary Flip!',
+                color: 15509525,
+                fields: [
+                    {
+                        name: 'Item:',
+                        value: `\`\`\`${itemName}\`\`\``,
+                        inline: true
+                    },
+                    {
+                        name: 'Bought for:',
+                        value: `\`\`\`${price}\`\`\``,
+                        inline: true
+                    },
+                    {
+                        name: 'Estimated Profit:',
+                        value: `\`\`\`${profit}\`\`\``,
+                        inline: false,
+                    },
+                    { name:' ', value:`<t:${(Date.now() / 1000).toFixed(0)}:R>`, inline: false}
+                ],
+                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` },
+                footer: {
+                    "text": "Edit Made by luizfernando13",
+                    "icon_url": "https://cdn.discordapp.com/avatars/591035486693359641/a363e32ab636d378d46de06a1e6d6616.png?size=2048"
+                  }
+            }
+        ]
+    })
 }
 
 export function sendWebhookItemSold(itemName: string, price: string, purchasedBy: string) {
@@ -87,6 +128,7 @@ export function sendWebhookItemSold(itemName: string, price: string, purchasedBy
         embeds: [
             {
                 title: 'Item Sold',
+                color: 14881033,
                 fields: [
                     {
                         name: 'Purchased by:',
@@ -100,11 +142,12 @@ export function sendWebhookItemSold(itemName: string, price: string, purchasedBy
                     },
                     {
                         name: 'Sold for:',
-                        value: `\`\`\`${price}\`\`\``,
+                        value: `\`\`\`${price}\`\`\`\n\n`,
                         inline: true
-                    }
+                    },
+                    { name:' ', value:`<t:${(Date.now() / 1000).toFixed(0)}:R>`, inline: false}
                 ],
-                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` }
+                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` },
             }
         ]
     })
@@ -119,6 +162,7 @@ export function sendWebhookItemListed(itemName: string, price: string, duration:
         embeds: [
             {
                 title: 'Item Listed',
+                color: 15335168,
                 fields: [
                     {
                         name: 'Listed Item:',
@@ -132,12 +176,100 @@ export function sendWebhookItemListed(itemName: string, price: string, duration:
                     },
                     {
                         name: 'AH Duration:',
-                        value: `\`\`\`${duration}h\`\`\``,
+                        value: `\`\`\`${duration}h\`\`\`\n\n`,
                         inline: false
-                    }
+                    },
+                    { name:' ', value:`<t:${(Date.now() / 1000).toFixed(0)}:R>`, inline: false}
                 ],
-                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` }
+                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` },
             }
         ]
     })
 }
+
+
+export function SendWebhookTotals(buyTotal: number, soldTotal: number): void {
+    if (!isWebhookConfigured()) {
+      return;
+    }
+    let ingameName = getConfigProperty('INGAME_NAME');
+  
+    sendWebhookData({
+      embeds: [
+        {
+          title: '**Last Session Statistics:**',
+          description: 'Items purchased and sold by the bot during the last session.',
+          color: 2410969,
+          fields: [
+            { name: 'Items purchased:', value: `\`\`\`${buyTotal}\`\`\`` , inline: true},
+            { name: 'Items sold:', value: `\`\`\`${soldTotal}\`\`\``, inline: true},
+          ],
+          thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` },
+          footer: {
+            "text": "Edit Made by luizfernando13",
+            "icon_url": "https://cdn.discordapp.com/avatars/591035486693359641/a363e32ab636d378d46de06a1e6d6616.png?size=2048"
+          }
+        },
+      ],
+    });
+  }
+
+
+  export function DisconnectWebwook(DisconnectReason: string) {
+    if (!isWebhookConfigured()) {
+        return
+    }
+    let ingameName = getConfigProperty('INGAME_NAME')
+    sendWebhookData({
+        embeds: [
+            {
+                title: 'Disconnected from the server',
+                color: 14881033,
+                fields: [
+                    { name: 'You got disconnected in the account:', value: `\`\`\`${ingameName}\`\`\``, inline: false },
+                    {
+                        name: 'Disconnected at:',
+                        value: `<t:${(Date.now() / 1000).toFixed(0)}:t>`,
+                        inline: false
+                    },
+                    { name: 'Reason:', value: `\`\`\`${DisconnectReason}\`\`\`` , inline: false},
+                ],
+                thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` },
+                footer: {
+                    "text": "Edit Made by luizfernando13",
+                    "icon_url": "https://cdn.discordapp.com/avatars/591035486693359641/a363e32ab636d378d46de06a1e6d6616.png?size=2048"
+                  }
+            }
+        ]
+    })
+}
+
+export function webhookInterval(buyTotal: number, soldTotal: number, flipsBedTotal: number, noBedsTotal: number, startedSession: string, purse: string): void {
+    if (!isWebhookConfigured()) {
+      return;
+    }
+    let ingameName = getConfigProperty('INGAME_NAME');
+  
+    sendWebhookData({
+      embeds: [
+        {
+          title: 'Current flipping session',
+          description: 'Session Stats update every 30 minutes.',
+          color: 2410969,
+          fields: [
+            { name: 'Items purchased:', value: `\`\`\`${buyTotal}\`\`\`` , inline: false},
+            { name: 'Items sold:', value: `\`\`\`${soldTotal}\`\`\``, inline: false},
+            { name: 'Winned Flips that are beds:', value: `\`\`\`${flipsBedTotal}\`\`\``, inline: false},
+            { name: 'Winned Flips that not are beds:', value: `\`\`\`${noBedsTotal}\`\`\``, inline: false},
+            { name: 'Purse:', value: `\`\`\`${purse}\`\`\``, inline: false},
+            { name: 'Session Started at:', value: `<t:${startedSession}:f> // <t:${startedSession}:R>`, inline: false},
+          ],
+          thumbnail: { url: `https://minotar.net/helm/${ingameName}/600.png` },
+          footer: {
+            "text": "Edit Made by luizfernando13",
+            "icon_url": "https://cdn.discordapp.com/avatars/591035486693359641/a363e32ab636d378d46de06a1e6d6616.png?size=2048"
+          }
+        },
+      ],
+    });
+  }
